8891ec7d7ccabbebdb5e270c002f7acd5efd8b7d
Debrin77 <priocas@protonmail.com>
2026-05-05 01:38:02 +0200

    as137

--- js/app-hc-pwa-fotodb.js
+++ js/app-hc-pwa-fotodb.js
@@ -9,7 +9,7 @@
 // Registrar Service Worker para PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js?v=2026-05-05-mobile-boot')
+    navigator.serviceWorker.register('service-worker.js?v=2026-05-05-pixel-tab-poc')
       .then(reg => console.log('[HidroCultivo] SW registrado:', reg.scope))
       .catch(err => console.warn('[HidroCultivo] SW error:', err));
   });
